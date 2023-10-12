b9c40322cecb26059c609b013685c7fdee6f2233
Angelo Rossi <angelo.rossi@epfl.ch>
2023-10-12 15:01:00 +0200

    added new helpers and document on features

--- implementations.py
+++ implementations.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     pass
 
@@ -5,10 +7,43 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     pass
 
 def least_squares(y, tx):
-    pass
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
+
+    w = np.linalg.solve(tx.T@tx, tx.T@y)
+    mse = 1/(2*len(y))*(y - tx@w).T@(y - tx@w)
+    return w, mse
 
 def ridge_regression(y, tx, lambda_):
-    pass
+    """implement ridge regression.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
+    array([ 0.21212121, -0.12121212])
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
+    array([0.03947092, 0.00319628])
+    """
+    lambda_prime = 2*len(y)*lambda_
+    return np.linalg.inv(tx.T@tx + lambda_prime*np.identity(tx.shape[1]))@tx.T@y
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     pass
---
+++ todo.md
@@ -0,0 +1,22 @@
+# All tasks todo
+
+- Exploratory data analysis and cleaning
+    - Clean out irrevelent features
+    - Decide on how we will treat NaNs
+    - One-hot encode binary variables, ex) country, file disposition
+    - Observe dataset distributions and identify confounders
+
+- Get basic model to work
+
+- Decide on how to feature engineer
+
+
+## Features
+
+CADULT - Are you 18 years of age or older?
+GENHLTH
+PHYSHLTH
+MENHLTH
+POORHLTH
+MEDCOST
+CHECKUP1
