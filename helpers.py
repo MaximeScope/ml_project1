Fixed logistic regression: + function to balance data + function to process features (onehot encode and normalise) + fixed prediction function (include sigmoid) + fixed scaling of y to be [0, 1] for training and [-1, 1] for final predictions

helpers.py
@@ -414,16 +414,23 @@ def first_filter(x_train, x_train_head, x_test, filter):
     return x_train_f1, x_test_f1
 
 
-def make_predictions(weights, x_test):
-    # Use weights to predict which columns correlate the most with y_train
+def make_predictions_linear_regression(weights, x_test):
     y_pred = x_test.dot(weights)
+
     # Transform the predictions with values from -1 to 1
     y_pred_norm = 2 * (y_pred - y_pred.min()) / (y_pred.max() - y_pred.min()) - 1
-    # If the value is above 0, consider it to be 1 and otherwise -1
+
     y_pred_norm[y_pred_norm > 0] = 1
     y_pred_norm[y_pred_norm <= 0] = -1
     
     return y_pred_norm
+
+def make_predictions_logistic_regression(weights, x_test):
+    y_pred = sigmoid(x_test.dot(weights))
+    y_pred[y_pred <= 0.5] = -1
+    y_pred[y_pred > 0.5] = 1
+    
+    return y_pred
 
 
 def create_csv_submission(ids, y_pred, name):
@@ -488,3 +495,93 @@ def second_filter(x_train, x_test, tol=1e-3):
         if not is_prop:
             cols_filtered.append(col1)
     return x_train[:, cols_filtered], x_test[:, cols_filtered]
+
+def process_labels(y):
+    y[y == -1] = 0
+    y[y == 1] = 1 
+    return y    
+
+def onehot_encode_col(x_trainj, x_testj):
+    unique = np.unique(x_trainj)
+    x_trainj_onehot = np.zeros((len(x_trainj), len(unique)))
+    x_testj_onehot = np.zeros((len(x_testj), len(unique)))
+
+    for j in range(len(x_trainj)):
+        x_trainj_onehot[j, np.where(unique == x_trainj[j])] = 1
+
+    for j in range(len(x_testj)):
+        x_testj_onehot[j, np.where(unique == x_testj[j])] = 1
+
+    return x_trainj_onehot, x_testj_onehot
+
+def standardize_col(x_trainj, x_testj):
+    mean = np.mean(x_trainj)
+    std = np.std(x_trainj)
+    x_trainj = (x_trainj - mean)/std + .5
+    x_testj = (x_testj - mean)/std + .5
+
+    return x_trainj.reshape(len(x_trainj), -1), x_testj.reshape(len(x_testj), -1)
+
+def process_features(x_train, x_test, onehot_thresh=100):
+    x_train_processed = np.zeros((x_train.shape[0], 0))
+    x_test_processed = np.zeros((x_test.shape[0], 0))
+
+    for j in range(x_train.shape[1]):
+        num_uniquej = len(np.unique(x_train[:, j]))
+
+        # onehot encode if number of unique values is less than onehot_thresh
+        if (num_uniquej <= onehot_thresh):
+            x_trainj_onehot, x_testj_onehot = onehot_encode_col(x_train[:, j], x_test[:, j])
+            x_train_processed = np.hstack((x_train_processed, x_trainj_onehot))
+            x_test_processed = np.hstack((x_test_processed, x_testj_onehot))
+            print(f'Feature index {j} has {num_uniquej} unique values --> onehot encoding')
+
+        else:  
+        # standardize if number of unique values is greater than onehot_thresh
+            x_trainj_standardized, x_testj_standardized = standardize_col(x_train[:, j], x_test[:, j])
+            x_train_processed = np.hstack((x_train_processed, x_trainj_standardized))
+            x_test_processed = np.hstack((x_test_processed, x_testj_standardized))
+            print(f'Feature index {j} has {num_uniquej} > {onehot_thresh} unique values --> standardizing')
+
+    return x_train_processed, x_test_processed
+
+def shuffle_data(x, y):
+    """Shuffle the data"""
+    shuffle_indices = np.random.permutation(np.arange(len(y)))
+    return x[shuffle_indices], y[shuffle_indices]
+
+def balance_data(x_train, y_train):
+    """
+    Balance the data such that there are equal number of y_train entries = 0 and y_train entries = 1
+    Args:
+        x_train: input data
+        y_train: output data (0 or 1)
+    Returns:
+        x_train_b: balanced input data
+        y_train_b: balanced output data
+    """
+    # Count number of y_train entries = 1
+    tot_ytrain1 = len(y_train[y_train == 1]) 
+
+    # Create data subset such that there are equal number of y_train entries = 0 and y_train entries = 1
+    x_train_b = []
+    y_train_b = []
+    cnt_ytrain1 = 0
+    cnt_ytrain0 = 0
+    for i in range(len(y_train)):
+        if (y_train[i] == 1):
+            x_train_b.append(x_train[i])
+            y_train_b.append(y_train[i])
+            cnt_ytrain1 += 1
+        elif (y_train[i] == 0):
+            if (cnt_ytrain0 < tot_ytrain1):
+                x_train_b.append(x_train[i])
+                y_train_b.append(y_train[i])
+                cnt_ytrain0 += 1
+
+    x_train_b = np.array(x_train_b)
+    y_train_b = np.array(y_train_b)
+    # Shuffle the data
+    x_train_b, y_train_b = shuffle_data(x_train_b, y_train_b)
+
+    return x_train_b, y_train_b 

implementations.py
@@ -103,24 +103,36 @@ def ridge_regression(y, tx, lambda_):
                            np.transpose(tx).dot(y))
 
 
-def logistic_regression(y, tx, initial_w, max_iters, gamma):
+def logistic_regression(y, tx, initial_w, gamma, max_iters=100, convergeance_thresh=1e-3):
     w = initial_w
-    loss = 0
+    prev_loss = 99999
     for iter in range(max_iters):
         # get loss and update w.
         loss, w = helpers.learning_by_newton_method(y, tx, w, gamma)
+        print(f'Current iteration={iter}, loss={loss}')
+
+        # convergeance check
+        if (prev_loss - loss) < convergeance_thresh:
+            print(f'Converged!')
+            break
+        else: 
+            prev_loss = loss
 
     return w, loss
 
 
-def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+def reg_logistic_regression(y, tx, initial_w, gamma, lambda_, max_iters=100, convergeance_thresh=1e-3):
     w = initial_w
-    loss = 0
+    prev_loss = 99999
     for iter in range(max_iters):
         # get loss and update w.
         loss, w = helpers.learning_by_penalized_gradient(y, tx, w, gamma, lambda_)
+        print(f'Current iteration={iter}, loss={loss}')
 
+        # convergeance check
+        if (prev_loss - loss) < convergeance_thresh:
+            print(f'Converged!')
+            break
+        else: 
+            prev_loss = loss
     return w, loss
-
-
-# I want a function that takes c_

run.py
@@ -1,6 +1,7 @@
 import numpy as np
 import helpers
 import implementations
+np.random.seed(42)
 
 # Manually selected list of features to delete:
 filter1 = ['GENHLTH', 'PHYSHLTH', 'MENTHLTH', 'POORHLTH', 'HLTHPLN1', 'PERSDOC2', 'MEDCOST', 'CHECKUP1', 'BPHIGH4', 'BPMEDS', 'BPMEDS', 'BLOODCHO', 'CHOLCHK', 'TOLDHI2', 'CVDSTRK3', 'ASTHMA3', 'ASTHNOW', 'ASTHMA3', 'CHCSCNCR', 'CHCCOPD1', 'HAVARTH3', 'ADDEPEV2', 'CHCKIDNY', 'DIABETE3', 'DIABAGE2', 'SEX', 'MARITAL', 'EDUCA', 'RENTHOM1', 'NUMHHOL2', 'CPDEMO1', 'VETERAN3', 'EMPLOY1', 'CHILDREN', 'INCOME2', 'WEIGHT2', 'HEIGHT3', 'PREGNANT', 'QLACTLM2', 'USEEQUIP', 'DECIDE', 'DIFFWALK', 'DIFFDRES', 'DIFFALON', 'SMOKE100', 'SMOKDAY2', 'STOPSMK2', 'LASTSMK2', 'USENOW3', 'ALCDAY5', 'AVEDRNK2', 'DRNK3GE5', 'MAXDRNKS', 'FRUITJU1', 'FRUIT1', 'FVBEANS', 'FVGREEN', 'FVORANG', 'VEGETAB1', 'EXERANY2', 'EXRACT11', 'EXEROFT1', 'EXERHMM1', 'STRENGTH', 'LMTJOIN3', 'ARTHDIS2', 'JOINPAIN', 'FLUSHOT6', 'FLSHTMY2', 'PNEUVAC3', 'HIVTST6', 'HIVTSTD3', 'PDIABTST', 'PREDIAB1', 'INSULIN', 'BLDSUGAR', 'FEETCHK2', 'DOCTDIAB', 'CHKHEMO3', 'DIABEYE', 'DIABEDU', 'CAREGIV1', 'CRGVREL1', 'CRGVPRB1', 'VINOCRE2', 'CIMEMLOS', 'WTCHSALT', 'LONGWTCH', 'DRADVISE', 'ASTHMAGE', 'ASATTACK', 'ASDRVIST', 'ASRCHKUP', 'ASACTLIM', 'ASYMPTOM', 'ASNOSLEP', 'ASTHMED3', 'ASINHALR', 'HAREHAB1', 'STREHAB1', 'CVDASPRN', 'ASPUNSAF', 'RLIVPAIN', 'RDUCHART', 'RDUCSTRK', 'ARTTODAY', 'ARTHWGT', 'ARTHEXER', 'ARTHEDU', 'TETANUS', 'HPVADVC2', 'SHINGLE2', 'HADMAM', 'HOWLONG', 'HADPAP2', 'LASTPAP2', 'HPVTEST', 'HPLSTTST', 'HADHYST2', 'PROFEXAM', 'LENGEXAM', 'BLDSTOOL', 'HADSIGM3', 'LASTSIG3', 'PCPSARE1', 'PSATEST1', 'PSATIME', 'SCNTMNY1', 'SCNTMEL1', 'SCNTWRK1', 'SCNTLWK1', 'SXORIENT', 'TRNSGNDR', 'EMTSUPRT', 'LSATISFY', 'ADPLEASR', 'ADDOWN', 'ADSLEEP', 'ADENERGY', 'ADEAT1', 'ADFAIL', 'ADTHINK', 'ADMOVE', 'MISTMNT', 'ADANXEV', 'QSTLANG', 'MSCODE', '_CHISPNC', '_DUALUSE', '_RFHLTH', '_RFHYPE5', '_PRACE1', '_MRACE1', '_RACE', '_AGEG5YR', '_BMI5CAT', '_CHLDCNT', '_EDUCAG', '_INCOMG', '_SMOKER3', '_RFSMOK3', 'DRNKANY5', 'DROCDY3_', '_RFBING5', '_DRNKWEK', '_RFDRHV5', '_FRTLT1', '_VEGLT1', '_TOTINDA', 'ACTIN11_', 'ACTIN21_', '_MINAC21', '_PACAT1', '_PAINDX1', '_PA150R2', '_PA300R2', '_PA30021', '_PASTRNG', '_PAREC1', '_LMTACT1', '_LMTWRK1', '_LMTSCL1', '_FLSHOT6', '_PNEUMO2', '_AIDTST3']
@@ -8,7 +9,13 @@ filter1 = ['GENHLTH', 'PHYSHLTH', 'MENTHLTH', 'POORHLTH', 'HLTHPLN1', 'PERSDOC2'
 ## 1. Load the training data into feature matrix and class labels
 x_train, x_train_head, x_test, y_train, train_ids, test_ids = helpers.load_csv_data("data")
 
-## 2. Filter the features:
+## 2. Convert labels from {-1, 1} to {0, 1}
+y_train_p = helpers.process_labels(y_train)
+
+## 3. Balance the data
+x_train, y_train_p = helpers.balance_data(x_train, y_train)
+
+## 4. Filter the features:
 # First filter
 x_train_f1, x_test_f1 = helpers.first_filter(x_train, x_train_head, x_test, filter1)
 
@@ -16,19 +23,29 @@ x_train_f1, x_test_f1 = helpers.first_filter(x_train, x_train_head, x_test, filt
 x_train_f1, x_test_f1 = helpers.replace_nan_with_median(x_train_f1, x_test_f1)
 
 # Second filter
-x_train_f2, x_test_f2 = helpers.second_filter(x_train_f1, x_test_f1)
+x_train_f2, x_test_f2 = helpers.second_filter(x_train_f1, x_test_f1, 1)
 
-## 3. Train the model:
+## 5. Encode the categorical features and standardize the data 
+x_train_p, x_test_p = helpers.process_features(x_train_f2, x_test_f2, 33)
+
+## 6. Train the model:
 # Weights from least squares
 # weights, _ = implementations.least_squares(y_train, x_train_f2)
 
 # Weights from ridge regression
-weights, rmse = helpers.train_model(y_train, x_train_f2, 2, 1, implementations.logistic_regression,
-                                    np.logspace(-4, 0, 5), np.ones(x_train_f2.shape[1]), 20)
-print("best w: " + str(weights) + " with rmse " + str(rmse))
+# weights, rmse = helpers.train_model(y_train, x_train_f2, 2, 1, implementations.logistic_regression,
+#                                     np.logspace(-4, 0, 5), np.ones(x_train_f2.shape[1]), 20)
+# print("best w: " + str(weights) + " with rmse " + str(rmse))
+weights, _ = implementations.reg_logistic_regression(y_train_p, 
+                                        x_train_p,
+                                        initial_w=np.zeros(x_train_p.shape[1]) + 1e-1, 
+                                        gamma=1e-2,
+                                        lambda_=1e-3,
+                                        max_iters=100,
+                                        convergeance_thresh=1e-3)
 
 # 4. Make predictions:
-y_pred_norm = helpers.make_predictions(weights, x_test_f2)
+y_pred = helpers.make_predictions_logistic_regression(weights, x_test_p)
 
 # Store the predictions in a submission_file.csv in CSV format without index_label
-helpers.create_csv_submission(test_ids, y_pred_norm, 'submission_file.csv')
+helpers.create_csv_submission(test_ids, y_pred, 'submission.csv')