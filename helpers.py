Fixed F1 score calculation

helpers.py
@@ -116,11 +116,12 @@ def ridge_regression_cross_validation(y, x, k_indices, k, lambda_):
 
 
 def calculate_fscore(y, x, w):
-    pred_y = make_predictions_logistic_regression(w, x)
+    pred_y = process_labels(make_predictions_logistic_regression(w, x))
     f_vec = y + 2*pred_y
     tp = np.count_nonzero(f_vec == 3)
     fp = np.count_nonzero(f_vec == 2)
     fn = np.count_nonzero(f_vec == 1)
+    print(f_vec)
     return tp / (tp + (fp + fn) / 2)
 
 

run.py
@@ -218,7 +218,7 @@ x_train_f2, x_test_f2 = helpers.second_filter(x_train_f1, x_test_f1, 1)
 print(f"applied second filter, remaining features: {x_train_f2.shape[1]}")
 
 ## 5. Encode the categorical features and standardize the data
-x_train_p, x_test_p = helpers.process_features(x_train_f2, x_test_f2, 33)
+x_train_p, x_test_p = helpers.process_features(x_train_f2, x_test_f2, 10)
 print(f'features processed, remaining features: {x_train_p.shape[1]}')
 
 ## 6. Train the model:
@@ -240,7 +240,7 @@ weights, f_score = helpers.train_model(
     1,
     implementations.logistic_regression,
     helpers.calculate_nll,
-    np.logspace(0.440625, 0.44375, 5),
+    np.logspace(-2, -1, 5),
     initial_w=np.zeros(x_train_p.shape[1]),
     # initial_w=(np.random.random(x_train_p.shape[1]) - 0.5)*10,
     max_iters=100,